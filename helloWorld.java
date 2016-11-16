1853d224c761cd97e68cb704321b8807ca509b9f
Justin <justin.oc03@gmail.com>
2016-11-15 21:51:56 -0600

    adding name

--- helloWorld.java
+++ helloWorld.java
@@ -2,5 +2,14 @@ public class helloWorld {
   public static void main(String[] args){
     System.out.println("Hello World!");
     System.out.println("This is a test");
+    System.out.println("            **   **         **         ** **                     ");
+    System.out.println("            **   **         **       **     **                            ");
+    System.out.println("            **   **         **         **                               ");
+    System.out.println("   **       **   **         **     **     **                              ");
+    System.out.println("    **    **      **       **        ** **                                  ");
+    System.out.println("      ****           *****                                                ");
+    System.out.println("     **                                                                                   ");
+    System.out.println("     **                                                                                 ");
+    System.out.println("     **                                                                                 ");
   }
 }
